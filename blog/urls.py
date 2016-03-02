93789efece16515f3d1ac82b68e39e4707a10fa0
David Velilla <david@neonplay.com>
2016-03-02 17:44:52 +0000

    User urls in Django 1.6 format

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url
-
+from django.conf.urls import patterns, url
 from . import controller
 
-app_name = 'blog'
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^posts$', controller.get_all_posts),
     url(r'^posts/(?P<post_id>[0-9]+)/comment$', controller.add_comment_to_post),
-]
+)
