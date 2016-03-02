ffacb104349eb5e854a02e93640d2675e24ef115
David Velilla <david@neonplay.com>
2016-03-02 10:15:00 +0000

    Use .status_code instead of .status, protect /posts from other HTTP methods than GET

--- blog/controller.py
+++ blog/controller.py
@@ -5,23 +5,31 @@ from .models import Post, Comment
 
 
 def get_all_posts(request):
-    posts = Post.objects.filter(published=True).prefetch_related('comments').order_by('-createdOn')
-    data = []
-    for post in posts:
-        data.append(post.serialize())
-    response = HttpResponse(json.dumps(data), content_type="application/json")
-    response['Access-Control-Allow-Credentials'] = 'true'
-    response['Access-Control-Allow-Headers'] = 'Content-Type'
+    response = HttpResponse(content_type="application/json")
     response['Access-Control-Allow-Origin'] = 'http://localhost'
+    response['Access-Control-Allow-Headers'] = 'Content-Type'
+    response['Access-Control-Allow-Credentials'] = 'true'
+
+    if request.method == 'GET':
+        posts = Post.objects.filter(published=True).prefetch_related('comments').order_by('-createdOn')
+        data = []
+        for post in posts:
+            data.append(post.serialize())
+        response.write(json.dumps(data))
+        response.status_code = 200
+    else:
+        response.status_code = 405
+
     return response
 
 
 def add_comment_to_post(request, post_id):
     response = HttpResponse(content_type="application/json")
     response['Access-Control-Allow-Origin'] = 'http://localhost'
-    response['Access-Control-Allow-Methods'] = 'POST'
     response['Access-Control-Allow-Headers'] = 'X-CSRFToken, Content-Type'
     response['Access-Control-Allow-Credentials'] = 'true'
+    response['Access-Control-Allow-Methods'] = 'POST'
+
     if request.method == 'OPTIONS':
         response.status = 200
     elif request.method == 'POST':
@@ -31,11 +39,11 @@ def add_comment_to_post(request, post_id):
             comment_body = json_body['body']
             comment_author = json_body['author']
             Comment.objects.create(post=post, author=comment_author, body=comment_body)
-            response.status = 201
+            response.status_code = 201
         except Post.DoesNotExist:
-            response.status = 404
+            response.status_code = 404
     else:
-        response.status = 405
+        response.status_code = 405
 
     return response
 
